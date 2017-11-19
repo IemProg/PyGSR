2b99b85e92680dc87d0558026ff905b8eeb69f5a
Imad Eddine MAROUF <btoxic24@gmail.com>
2017-11-19 16:14:49 +0100

    Using urllib instead of urllib2
    Since urllib2 has a lot of issues, no longer compatible with Py3

--- pygsr.py
+++ pygsr.py
@@ -1,30 +1,32 @@
-""" Simple way to access google api for speech recognition with python
+"""
+Simple way to access google api for speech recognition with python
 
 Copyright (c) 2013, Carlos Ganoza Plasencia
-url: http://carlosganoza.com
+Edited by : Iem-Prog (19/Nov/17)
+url: http://about.me/drneox
 """
 from pyaudio import PyAudio, paInt16
 from wave import open as open_audio
-from urllib2 import Request, urlopen
+#from urllib2 import Request, urlopen
+import urllib.request
 from os import system
 from json import loads
-import urllib2
+
 
 class Pygsr:
     def __init__(self, file="audio"):
         self.format = paInt16
-        self.rate = 48000
+        self.rate = 8000
         self.channel = 1
         self.chunk = 1024
         self.file = file
 
     def convert(self):
-        system("sox %s -t wav -r 48000 -t flac %s.flac" % (self.file, self.file))
+        system("sox %s -t wav -r 8000 -t flac %s.flac" % (self.file, self.file))
 
-    def record(self, time, device_i=None):
+    def record(self, time):
         audio = PyAudio()
-        print audio.get_device_info_by_index(1)
-        stream = audio.open(input_device_index=device_i,output_device_index=device_i,format=self.format, channels=self.channel,
+        stream = audio.open(format=self.format, channels=self.channel,
                             rate=self.rate, input=True,
                             frames_per_buffer=self.chunk)
         print("REC: ")
@@ -33,7 +35,6 @@ class Pygsr:
             data = stream.read(self.chunk)
             frames.append(data)
         stream.stop_stream()
-        print("END")
         stream.close()
         audio.terminate()
         write_frames = open_audio(self.file, 'wb')
@@ -48,15 +49,9 @@ class Pygsr:
         url = "http://www.google.com/speech-api/v1/recognize?lang=%s" % language
         file_upload = "%s.flac" % self.file
         audio = open(file_upload, "rb").read()
-        header = {"Content-Type": "audio/x-flac; rate=48000"}
-        data = Request(url, audio, header)
-        try:
-            post = urlopen(data)
-            response = post.read()
-            phrase = loads(response)['hypotheses'][0]['utterance']
-            return phrase, response
-        except urllib2.HTTPError, err:
-            if err.code == 400:
-                 return "Error: No se pudo reconocer", "400"
-
-	
+        header = {"Content-Type": "audio/x-flac; rate=8000"}
+        data = urllib.request(url, audio, header)
+        post = urllib.request.urlopen(data)
+        response = post.read()
+        phrase = loads(response)['hypotheses'][0]['utterance']
+        return phrase, response
